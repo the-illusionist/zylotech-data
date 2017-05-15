5edc8718410c0daa922d83851f85c23ab075e436
Abhishek kumar <abhishek.rkmv5@gmail.com>
2017-05-15 17:57:24 +0530

    insert into both tables using same id

--- insert.js
+++ insert.js
@@ -9,19 +9,28 @@ MongoClient.connect(url, function(err, db) {
 	// var bulk = db.collection('product').initializeUnorderedBulkOp();     //Bulk & insertMany both have smae implementation internally
 	var alphArr = ["a","b","c","d","e"];
 	var totalRecords = 1000000;
-	var arr = []
+	var arrProd = []
+	var arrTran = []
 	var i=0;
 	while(i<totalRecords){
 		uId = Math.random().toString().slice(2,12);
 		index = Math.floor(Math.random()*5);
-		record = {"user_id":uId,"Y":alphArr[index]};
-		// bulk.insert(record);
-		arr.push(record);
+		recordProd = {"user_id":uId,"Y":alphArr[index]};
+		// bulk.insert(recordProd);
+		arrProd.push(recordProd);
+
+		xVal = Math.random().toString().slice(2,7);
+		zVal = Math.random().toString().slice(2,8);
+		recordTran = {"user_id":uId, "X":xVal , "Z":zVal};
+		// bulk.insert(recordTran);
+		arrTran.push(recordTran);
+
 		i++;
 		if(i%100000 == 0)
 			console.log(i + " inserted");
 	}
 	// bulk.execute();
-	db.collection('product3').insertMany( arr )
+	db.collection('product3').insertMany( arrProd );
+	db.collection('transaction3').insertMany( arrTran );
 	// db.close();
 });
